edeef0f3a4700caa1c78e42bc553b2be6e004d36
Srinithy676 <sp7324@srmist.edu.in>
2026-03-18 09:54:59 +0530

    sri added UC1-WelcomePage

---
+++ APP/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,16 @@
+/** MAIN CLASS-UseCase1HotelBookingApp
+ * Use Case 1 :Application Entry & Welcome Message
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+public class UseCase1HotelBookingApp{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to Hotel Booking Management System \nSystem initialized successfully");
+    }
+
+}
